25a647aba6da972719a77c698e303d8f9c41f6dd
Евгений Кучма <desferto87@gmail.com>
2024-12-23 00:23:38 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace homeWorkPowerTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countPower = 0;
+            int numberPowerTwo =2;
+            int numberTwo = 2;
+            int givenNumber;
+            int minValue = 2;
+            int maxValue = 100;
+            bool isRunning = true;
+
+            Random random = new Random();
+            givenNumber = random.Next(minValue,maxValue);
+
+            while (isRunning)
+            {
+                numberPowerTwo *= numberTwo;
+                countPower++;
+
+                if (numberPowerTwo > givenNumber)
+                { 
+                isRunning = false;
+                }
+            }
+
+            Console.WriteLine(givenNumber);
+            Console.WriteLine(numberPowerTwo);
+            Console.WriteLine(countPower);
+        }
+    }
+}
